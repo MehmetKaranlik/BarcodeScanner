c6f25d41d557b5197e07ae4aae8d7ace4f409f74
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2021-12-20 19:57:55 +0300

    ss

--- flutter_barcode_scanner/flutter_bc_scanner/lib/feature/view/home/view/home_view.dart
+++ flutter_barcode_scanner/flutter_bc_scanner/lib/feature/view/home/view/home_view.dart
@@ -1,12 +1,18 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: ,
+      ),
+    );
   }
 }
